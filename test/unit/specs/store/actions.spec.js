5b20fa6ff7b5a0daa9546681abc7632519450625
Maxence POUTORD <maxence.poutord@gmail.com>
2017-09-12 13:40:00 +0100

    :hammer: separate testAction

--- test/unit/specs/store/actions.spec.js
+++ test/unit/specs/store/actions.spec.js
@@ -1,39 +1,7 @@
-import { expect } from 'chai'
 import { addToSelection, removeFromSelection } from '@/store/actions'
+import { testAction } from '../../custom/test-actions'
 
 describe('store - actions', () => {
-  const testAction = (action, payload, state, expectedMutations, done) => {
-    let count = 0
-
-    // mock commit
-    const commit = (type, payload) => {
-      const mutation = expectedMutations[count]
-
-      try {
-        expect(mutation.type).to.equal(type)
-        if (payload) {
-          expect(mutation.payload).to.deep.equal(payload)
-        }
-      } catch (error) {
-        done(error)
-      }
-
-      count++
-      if (count >= expectedMutations.length) {
-        done()
-      }
-    }
-
-    // call the action with mocked store and arguments
-    action({ commit, state }, payload)
-
-    // check if no mutations should have been dispatched
-    if (expectedMutations.length === 0) {
-      expect(count).to.equal(0)
-      done()
-    }
-  }
-
   it('should loadStarships', (done) => {
     const starship = {
       name: 'X-Wing'
