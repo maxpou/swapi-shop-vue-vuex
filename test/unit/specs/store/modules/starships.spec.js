1170605e0e5207b6d71babce315940a5aef92c2b
Maxence POUTORD <maxence.poutord@gmail.com>
2017-09-12 13:40:00 +0100

    :hammer: separate testAction

--- test/unit/specs/store/modules/starships.spec.js
+++ test/unit/specs/store/modules/starships.spec.js
@@ -2,6 +2,7 @@ import { expect } from 'chai'
 import starshipsStore from '@/store/modules/starships'
 const actionsInjector = require('inject-loader!@/store/modules/starships')
 import swapiResponse from '../../api/swapi.response.json'
+import { testAction } from '../../../custom/test-actions'
 
 describe('starships store - getters', () => {
   it('return all starship', () => {
@@ -77,38 +78,6 @@ describe('starships store - actions', () => {
       }
     }
   })
-
-  const testAction = (action, payload, state, expectedMutations, done) => {
-    let count = 0
-
-    // mock commit
-    const commit = (type, payload) => {
-      const mutation = expectedMutations[count]
-
-      try {
-        expect(mutation.type).to.equal(type)
-        if (payload) {
-          expect(mutation.payload).to.deep.equal(payload)
-        }
-      } catch (error) {
-        done(error)
-      }
-
-      count++
-      if (count >= expectedMutations.length) {
-        done()
-      }
-    }
-
-    // call the action with mocked store and arguments
-    action({ commit, state }, payload)
-
-    // check if no mutations should have been dispatched
-    if (expectedMutations.length === 0) {
-      expect(count).to.equal(0)
-      done()
-    }
-  }
 
   it('should loadStarships', (done) => {
     const pageNum = 1
